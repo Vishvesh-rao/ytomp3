c220a2584d11d4aae9cd9001c678535f50103da1
Vishvesh a.k.a Str1d3r <vishveshsrao@gmail.com>
2021-06-14 17:33:48 +0530

    Update mp3dldr.py

--- mp3dldr.py
+++ mp3dldr.py
@@ -20,7 +20,7 @@ def mp3downloader(fname,vid_id):
 
         with open('file.mp3', 'rb') as audio:
             payload = {
-                'chat_id': 972187028,
+                'chat_id': chatid,   ## input chat id
                 'title': '{filename}.mp3'.format(filename=filename),
                 'parse_mode': 'HTML'
             }
